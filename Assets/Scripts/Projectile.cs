3113199788ea43392953e6b5ecb358de827c3cc3
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-08-02 17:30:52 +0900

    Create Nav, Destroy Bullet

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
     float speed = 10;
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);    
+        float moveDistance = speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * Time.deltaTime * moveDistance);    
+    }
+
+    void CheckCollisions(float moveDistance)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        GameObject.Destroy(gameObject);
     }
 }
